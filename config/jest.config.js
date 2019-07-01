35f653a110b04d9bae49ec15118557435c59aaa7
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-07-01 16:11:44 -0500

    added a redirect for home index

--- config/jest.config.js
+++ config/jest.config.js
@@ -5,15 +5,10 @@ module.exports = {
   moduleDirectories: ["node_modules", "bower_components", "shared"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
   transform: {
     "^.+\\.js$": "babel-jest",
     ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
-  },
-
-  // moduleNameMapper: {
-  //   "^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
-  // },
-  // transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  }
 }
--- server/router.js
+++ server/router.js
@@ -8,4 +8,7 @@ router.get('/pictures', (req, res) => {
   })
 })
 
+router.get('/', (req, res) => {
+  res.redirect('/items/1')
+})
 module.exports = router;
