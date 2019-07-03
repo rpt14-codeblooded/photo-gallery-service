4768818b3b25ff897cd7ef07674f441010d28191
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-07-03 07:50:39 -0500

    refactored database tests so that it's testing the db in a pure fashion

--- __tests__/database.test.js
+++ __tests__/database.test.js
@@ -22,3 +22,33 @@ describe("Database", () => {
     db.get(callback);
   })
 })
+
+const {MongoClient} = require('mongodb');
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect("mongodb://localhost/gallery", {
+      useNewUrlParser: true,
+    });
+    db = await connection.db("gallery");
+  });
+
+  afterAll(async () => {
+    await db.dropCollection('users');
+    await connection.close();
+    await db.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
